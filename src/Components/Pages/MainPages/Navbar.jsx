ab65d2363f843988522647ac6fbc07ce2b4d257d
Muzaffar Karimov <karimovweb.for.github@gmail.com>
2023-06-01 20:21:09 +0500

    Home page Tugadi

--- src/Components/Pages/MainPages/Navbar.jsx
+++ src/Components/Pages/MainPages/Navbar.jsx
@@ -1,8 +1,13 @@
 // Imgs
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Context } from "../../Context";
+
+// React Icons
+
+import {AiOutlineMenu} from "react-icons/ai"
+import { GrClose } from "react-icons/gr"
 
 export function Navbar() {
   let { lang, setLang } = useContext(Context);
@@ -10,6 +15,12 @@ export function Navbar() {
   let langFunc = () => {
     setLang(!lang);
   };
+
+  let [open,setOpen] = useState(false)
+
+  let openFunc = ()=>{
+    setOpen(!open)
+  }
 
   return (
     <div className="Navbar">
@@ -19,6 +30,16 @@ export function Navbar() {
         <NavLink to={"/xizmatlar"}>{lang ? "XIZMATLAR" : "услуги"} </NavLink>
         <NavLink to={"/galereya"}>{lang ? "GALEREYA" : "галерея"} </NavLink>
         <NavLink to={"/murojatuchun"}>{lang ? "MUROJAT UCHUN" : "для справки"}</NavLink>
+      </div>
+      <div className={open ? "nav__Router-phone-mode active" : "nav__Router-phone-mode"}>
+          <button className={open ? "phone-mode__nav-btn active" : "phone-mode__nav-btn"} onClick={openFunc}>{open ? <GrClose/> : <AiOutlineMenu/>}</button>
+          <div className="nav__Router-phone__mode">
+        <NavLink to={"/"}>{lang ? "BOSH SAHIFA" : "домашняя страница"}</NavLink>
+        <NavLink to={"/bizhaqimizda"}>{lang ? "BIZ HAQIMIZDA" : "о нас"}</NavLink>
+        <NavLink to={"/xizmatlar"}>{lang ? "XIZMATLAR" : "услуги"} </NavLink>
+        <NavLink to={"/galereya"}>{lang ? "GALEREYA" : "галерея"} </NavLink>
+        <NavLink to={"/murojatuchun"}>{lang ? "MUROJAT UCHUN" : "для справки"}</NavLink>
+      </div>
       </div>
       <div onClick={langFunc} className="nav__Language-Btn">
         {lang ? "uz" : "ru"}
